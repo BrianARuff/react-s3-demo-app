c73bc2dd86eee56d7f6122447c70d49e713ef150
Brian Ruff <briananthonyruff@gmail.com>
2023-01-18 04:29:01 -0500

    remove usePrevous hook

--- src/App.tsx
+++ src/App.tsx
@@ -154,16 +154,3 @@ const Spinner = () => {
     <div className="loader"></div>
   )
 }
-
-// Hook
-function usePrevious(value: any) {
-  // The ref object is a generic container whose current property is mutable ...
-  // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef();
-  // Store current value in ref
-  useEffect(() => {
-    ref.current = value;
-  }, [value]); // Only re-run if value changes
-  // Return previous value (happens before update in useEffect above)
-  return ref.current;
-}
